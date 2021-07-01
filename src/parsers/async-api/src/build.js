3d8009cc7de97626242f0fcc509064bb1046c34a
David Boyne <dave@deadhappy.com>
2021-07-01 13:19:35 +0100

    tmp

--- src/parsers/async-api/src/build.js
+++ src/parsers/async-api/src/build.js
@@ -1,6 +1,6 @@
 export default async ({ exec }) => {
   await exec(
-    `cd generated-docs/asyncapi && npx @asyncapi/generator events.yml @asyncapi/html-template -o ../../docs-html/asyncapi`,
+    `cd generated-docs/asyncapi && npx @asyncapi/generator@1.7.3 events.yml @asyncapi/html-template -o ../../docs-html/asyncapi`,
     true
   )
 }
